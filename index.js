5bf6dc19a307dbdb8a03babf8cb0c622d044b318
Wade Austin <wade.austin@gmail.com>
2019-02-11 21:53:13 -0600

    update header

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-    <Header />
+    <Header headerText={'Album'} />
 );
 
 // Render it to the device
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,33 @@
 // Import Libs for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
-    return <Text>Albums!</Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 80,
+        paddingTop: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative',
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 // Make the component available to other parts of the app
