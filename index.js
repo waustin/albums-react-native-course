7a8ea7f647a99b390485a9330b3c762295c83de7
Wade Austin <wade.austin@gmail.com>
2019-02-11 20:56:08 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+    <Header />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+
+// Import a library to help create component
+/*
+
+
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+
+*/
